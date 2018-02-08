0a7495d94bd21b454927aed18f75a1f33564c7d4
Porkchop5000 <35056109+Porkchop5000@users.noreply.github.com>
2018-02-07 17:24:41 -0800

    Add files via upload

---
+++ Prog109/Module05/Practice01/week5.js
@@ -0,0 +1,5 @@
+alert('Welcome User')
+var firstName = prompt('What  is your first name?');
+var lastName = prompt('Thank you ' + firstName +', what is your last name?');
+var message = 'Thank you' +firstName+' '+lastName+'. I now have your soul. Bwahahahahaha!!!';
+document.write('<h3>' + message + '</h3>');
