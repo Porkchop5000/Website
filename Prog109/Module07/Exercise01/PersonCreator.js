0e6ea9abd4033ef3fc0ab97530273aaeabba7ac9
Porkchop5000 <35056109+Porkchop5000@users.noreply.github.com>
2018-02-21 09:04:41 -0800

    Add files via upload

---
+++ Prog109/Module07/Exercise01/PersonCreator.js
@@ -0,0 +1,13 @@
+function Person (gender, age, weight, height) {
+	this.gender = gender;
+	this.age = age;
+	this.weight = weight;
+	this.height = height;
+}
+var Josh = new Person('male', 39, 151, 6);
+var Bob = new Person('male', 25, 168, 5.5);
+
+var people = [Josh, Bob];
+
+
+
