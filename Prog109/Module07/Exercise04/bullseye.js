2515fc3b987240b68143105604ecf53aa565ab0a
Porkchop5000 <35056109+Porkchop5000@users.noreply.github.com>
2018-02-25 17:41:19 -0800

    Add files via upload

---
+++ Prog109/Module07/Exercise04/bullseye.js
@@ -0,0 +1,30 @@
+var table = prompt("Enter a number 0-10");// Unit of table
+do {
+if ((isNaN(table)) || (table > 10) || (table < 0)) {
+	table = prompt("Please enter a valid number 0-10");
+}
+} while ((isNaN(table)) || (table > 10) || (table < 0));
+var operator = prompt("Enter addition or multiplication"); // Type of calculation
+msg = generateTable(table,operator);//this calls the function
+function generateTable(base, operator) {
+	var i = 1;                 // Set counter to 1
+	var msg = '';              // Message
+	if (operator === 'addition') {
+  // Do addition
+	while (i < 11) {
+    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+    i++;
+	}
+	} else {
+  // Do multiplication
+	while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+	}
+}
+return msg
+}
+
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
