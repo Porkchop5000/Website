485b8b179c335e278dc2e8a4b9bbb6528809e5db
Porkchop5000 <35056109+Porkchop5000@users.noreply.github.com>
2018-03-04 15:14:31 -0800

    Update form.js

--- Prog109/Module08/Artifact04/form.js
+++ Prog109/Module08/Artifact04/form.js
@@ -1,38 +1,129 @@
-function ValidateForm(){
- var validUsername = false;
- var validUserPassword = false;
- var errorMessages ="";  // All the error messages are going to stay in this variable
- /*********** VALIDATES USERNAME ******** */
- //Required field
-//This syntax is using name-of-form.name-of-field.value
-// You can also use document.getElementById("id-of-field").value
-
-/*********** VALIDATES USERNAME ******** */
-//required 12 characters or less
- 
- if (myContact.userName.value.length > 12 
- || myContact.userName.value === null 
-|| myContact.userName.value === "") {
- errorMessages += "<p>The user name must be 12 characters or less and it is required</p>"; 
+function validateEmail() {
+    var x = document.getElementById("email").value;
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+        alert("Not a valid e-mail address");
+        return false;
+    }
+	else{
+		return true;
+	}		
 }
- else 
- {
- validUsername = true;
- }
- //console.log(validUsername);  
- /*********** VALIDATES PASSWORD ********/
- if (myContact.password.value==null ||
- myContact.password.value=== "" ||
- myContact.password.value.length >15){
- errorMessages += "<p>The password must be less than 7 characters and it is required</p>";
- validUserPassword = false;
- }
- else{
- validUserPassword = true; 
- }
 
- document.getElementById("errorMessages").innerHTML = errorMessages;
+function ValidateForm(){
+var validUsername = false;
+var validPassword = false;
+var validFirstName = false;
+var validLastName = false;
+var validPhone = false;
+var validAddress = false;
+var validCity = false;
+var validCountry = false;
+var validZip = false;
+var validEmail = validateEmail();
+
+
+var errorMessages = "";//error message to be filled if mistakes made
+
+//validates first name 20 char or less. must be alphabetical
+if (myContact.firstname.value==null 
+	|| myContact.firstname.value=== "" 
+	|| myContact.firstname.value.length >20){
+	errorMessages += "<p>The first name must be less than 20 characters and is required.</p>";
+	}
+else{
+	validFirstName = true; 
+	}
+//validate last name 50 char or less. must be alphabetical
+if (myContact.lastname.value==null 
+	|| myContact.lastname.value=== "" 
+	|| myContact.lastname.value.length >50){
+	errorMessages += "<p>The last name must be less than 50 characters and is required.</p>";
+	}
+else{
+	validLastName = true; 
+	}
+	
+
+//Validate User Name - 12 char or less
+if (myContact.username.value.length > 12 
+	|| myContact.username.value===null 
+	|| myContact.username.value===""){
+	errorMessages += "<p>The user name must be less than 12 characters and is required.</p>";
+	}
+else {
+	validUsername =true;
+	}
+
+//validates password - 7 char or less 
+if (myContact.password.value==null 
+	|| myContact.password.value=== "" 
+	|| myContact.password.value.length >7){
+	errorMessages += "<p>The password must be less than 7 characters and is required.</p>";
+	}
+else{
+	validPassword = true; 
+	}
+
+//validates phone number - max 15 digits, must be a number
+if (myContact.phone.value==null 
+	|| myContact.phone.value=== "" 
+	|| myContact.phone.value.length >14
+	|| isNaN(myContact.phone.value)){
+	errorMessages += "<p>The phone number must be less than 14 characters, must be a number, and is required.</p>";
+	}
+else{
+	validPhone = true; 
+	}
+
+//validates address - must have something in it
+if (myContact.address.value==null 
+	|| myContact.address.value=== ""){
+	errorMessages += "<p>A street address is required.</p>";
+	}
+else{
+	validAddress = true; 
+	}
+
+//validates city - must have something in it
+if (myContact.city.value==null 
+	|| myContact.city.value=== ""){
+	errorMessages += "<p>A city is required.</p>";
+	}
+else{
+	validAddress = true; 
+	}
+
+//validates country - must be Canada, Mexico, or the United States
+if (myContact.countries.value==null 
+	|| myContact.countries.value=== ""
+	|| myContact.countries.value=== "Select one..."){
+	errorMessages += "<p>A country is required.</p>";
+	}
+else{
+	validCountry = true; 
+	}
+//validates zip code - only if USA is selected
+if (myContact.countries.value==="United States"){
+	if (myContact.zipcode.value==null 
+	|| myContact.zipcode.value=== "" 
+	|| myContact.zipcode.value.length >5
+	|| isNaN(myContact.zipcode.value)){
+	errorMessages += "<p>The zip code must be less than 5 characters, must be a number, and is required for any address in the United States.</p>";
+	}
+	else{
+	validZip = true;
+	}
+}
+else{
+	validZip = true;
+}
+
+//error message and return here
+document.getElementById("errorMessages").innerHTML = errorMessages;
 // Make sure you return all the boolean variables that are checking each field
- return (validUsername && validUserPassword) ;
-
+ return (validUsername && validPassword && validFirstName  && validLastName 
+	&& validEmail && validPhone && validAddress && validCity && validCountry
+	&& validZip) ;
 }
