ccafccee16544f0d1d664a150ad31cb7434e1d75
Porkchop5000 <35056109+Porkchop5000@users.noreply.github.com>
2018-03-08 21:18:13 -0800

    Add files via upload

---
+++ Prog109/Module09/Artifact5/slideshow.js
@@ -0,0 +1,47 @@
+var myImages =["images/fatty.jpg", "images/flippy.jpg","images/frogApe.jpg","images/clown.jpg","images/bigHands.jpg"];
+
+
+var captionImages =["Fatty","Flippy","Froggy","Clowny","Handy"];
+
+ var index=0; 
+
+ function updateImage(){
+ document.getElementById("slideshow").src = myImages[index];
+ document.getElementById("slideshow").alt= captionImages[index];
+ document.getElementById("caption").textContent = captionImages[index]; 
+} 
+
+function next(){
+ if (myImages.length == index+1)
+ index=0;
+ else
+ index++;
+ updateImage();
+} 
+ 
+
+function back(){
+ if (index===0)
+ index=myImages.length-1;
+ else
+ index--;
+ 
+ updateImage();
+} 
+
+
+function autoSlide(){
+if (document.getElementById("auto").checked)
+ next(); 
+}
+
+
+setInterval(autoSlide,2000); // Next
+
+
+var nextButton = document.getElementById("next"); 
+var previousButton = document.getElementById("previous"); 
+
+previousButton.addEventListener("click",back,false);
+nextButton.addEventListener("click",next,false); 
+
